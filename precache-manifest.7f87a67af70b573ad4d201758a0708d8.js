4f28f65787263965612e256c4505caa51de0e5d5
pgb4 <preston.bebas1@gmail.com>
2019-03-20 14:30:19 -0400

    Updates

---
+++ precache-manifest.7f87a67af70b573ad4d201758a0708d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsfinal/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "52a2f648c6c43790cb03",
+    "url": "/robofriendsfinal/static/js/runtime~main.52a2f648.js"
+  },
+  {
+    "revision": "e164d09b27122b594e93",
+    "url": "/robofriendsfinal/static/js/main.e164d09b.chunk.js"
+  },
+  {
+    "revision": "ee8f18bb3f1edf8cfc9f",
+    "url": "/robofriendsfinal/static/js/1.ee8f18bb.chunk.js"
+  },
+  {
+    "revision": "e164d09b27122b594e93",
+    "url": "/robofriendsfinal/static/css/main.46d472c0.chunk.css"
+  },
+  {
+    "revision": "ee8f18bb3f1edf8cfc9f",
+    "url": "/robofriendsfinal/static/css/1.c48ae67f.chunk.css"
+  },
+  {
+    "revision": "a137e07986e812206499ce146b3e42ad",
+    "url": "/robofriendsfinal/index.html"
+  }
+];
